b645a05d71184b2f636ffb663df994ca77a9536c
Young Mun Chae <dudans1536@nate.com>
2021-04-21 19:11:34 +0900

    Invalid 어쩌구 때문에 커밋 안돼서 싹 밈

--- app/src/main/java/kr/kpu/ac/kr/game/s2015180040/myapplication/Game/GameObject.java
+++
@@ -1,8 +0,0 @@
-package kr.kpu.ac.kr.game.s2015180040.myapplication.Game;
-
-import android.graphics.Canvas;
-
-public interface GameObject {
-    public void update();
-    public void draw(Canvas canvas);
-}
