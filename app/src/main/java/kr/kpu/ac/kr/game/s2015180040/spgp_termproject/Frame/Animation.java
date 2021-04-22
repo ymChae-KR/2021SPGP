fde014c544743e2294fce3bb2505d5a77a96eb40
Young Mun Chae <dudans1536@nate.com>
2021-04-22 17:30:47 +0900

    testing, 백업 파일 커밋

---
+++ app/src/main/java/kr/kpu/ac/kr/game/s2015180040/spgp_termproject/Frame/Animation.java
@@ -0,0 +1,73 @@
+package kr.kpu.ac.kr.game.s2015180040.spgp_termproject.Frame;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+
+import kr.kpu.ac.kr.game.s2015180040.spgp_termproject.UI.GameView;
+
+public class Animation extends GameBitmap {
+    //private Bitmap bitmap;
+    private final int imageWidth;
+    private final int imageHeight;
+    private final int frameWidth;
+    private final long createdOn;
+    private int frameIndex;
+    private final float framesPerSecond;
+    private final int frameCount;
+
+    protected Rect srcRect = new Rect();
+
+    public Animation(int resId, float framesPerSecond, int frameCount) {
+        super(resId);
+        //bitmap = GameBitmap.load(resId);
+        imageWidth = bitmap.getWidth();
+        imageHeight = bitmap.getHeight();
+        if (frameCount == 0) {
+            frameCount = imageWidth / imageHeight;
+        }
+        frameWidth = imageWidth / frameCount;
+        this.framesPerSecond = framesPerSecond;
+        this.frameCount = frameCount;
+        createdOn = System.currentTimeMillis();
+        frameIndex = 0;
+    }
+
+    //    public void update() {
+//        int elapsed = (int)(System.currentTimeMillis() - createdOn);
+//        frameIndex = Math.round(elapsed * 0.001f * framesPerSecond) % frameCount;
+//    }
+
+    public void draw(Canvas canvas, float x, float y) {
+        int elapsed = (int)(System.currentTimeMillis() - createdOn);
+        frameIndex = Math.round(elapsed * 0.001f * framesPerSecond) % frameCount;
+
+        int fw = frameWidth;
+        int h = imageHeight;
+        float hw = fw / 2 * GameView.MULTIPLIER;
+        float hh = h / 2 * GameView.MULTIPLIER;
+        srcRect.set(fw * frameIndex, 0, fw * frameIndex + fw, h);
+        dstRect.set(x - hw, y - hh, x + hw, y + hh);
+        canvas.drawBitmap(bitmap, srcRect, dstRect, null);
+    }
+
+    public int getWidth() {
+        return frameWidth;
+    }
+
+    public int getHeight() {
+        return imageHeight;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/kr/kpu/ac/kr/game/s2015180040/spgp_termproject/Game/Player.java
@@ -0,0 +1,60 @@
+package kr.kpu.ac.kr.game.s2015180040.spgp_termproject.Game;
+
+import android.graphics.Canvas;
+import android.graphics.RectF;
+
+import kr.kpu.ac.kr.game.s2015180040.spgp_termproject.Frame.GameBitmap;
+import kr.kpu.ac.kr.game.s2015180040.spgp_termproject.Frame.main;
+import kr.kpu.ac.kr.game.s2015180040.spgp_termproject.R;
+
+public class Player implements GameObject {
+    private static final String TAG = Player.class.getSimpleName();
+    private static final int BULLET_SPEED = 1500;
+    private static final float FIRE_INTERVAL = 1.0f / 7.5f;
+    private static final float LASER_DURATION = FIRE_INTERVAL / 3;
+    private float fireTime;
+    private float x, y;
+    private float tx, ty;
+    private float speed;
+    private GameBitmap playerBitmap;
+    private GameBitmap fireBitmap;
+
+    public Player(float x, float y) {
+        this.x = x;
+        this.y = y;
+        this.tx = x;
+        this.ty = 0;
+        this.speed = 800;
+        this.playerBitmap = new GameBitmap(R.mipmap.cookie);
+
+        this.fireTime = 0.0f;
+    }
+
+    public void moveTo(float x, float y) {
+        this.tx = x;
+        //this.ty = this.y;
+    }
+
+    public void update() {
+        main game = main.get();
+        float dx = speed * game.frameTime;
+        if (tx < x) { // move left
+            dx = -dx;
+        }
+        x += dx;
+        if ((dx > 0 && x > tx) || (dx < 0 && x < tx)) {
+            x = tx;
+        }
+
+        fireTime += game.frameTime;
+
+        }
+
+    public void draw(Canvas canvas) {
+        playerBitmap.draw(canvas, x, y);
+        if (fireTime < LASER_DURATION) {
+            fireBitmap.draw(canvas, x, y - 50);
+        }
+    }
+
+}
