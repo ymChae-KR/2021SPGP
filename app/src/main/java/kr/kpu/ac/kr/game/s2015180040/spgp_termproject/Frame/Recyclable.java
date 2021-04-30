321973b3c9bf2ddff3adc226612d35ba003e6970
Young Mun Chae <dudans1536@nate.com>
2021-04-30 21:47:51 +0900

    갈아 엎기

--- app/src/main/java/kr/kpu/ac/kr/game/s2015180040/spgp_termproject/Frame/Recyclable.java
+++
@@ -1,5 +0,0 @@
-package kr.kpu.ac.kr.game.s2015180040.spgp_termproject.Frame;
-
-public interface Recyclable {
-    public void recycle();
-}
--- app/src/main/java/kr/kpu/ac/kr/game/s2015180040/spgp_termproject/Game/GameObject.java
+++
@@ -1,8 +0,0 @@
-package kr.kpu.ac.kr.game.s2015180040.spgp_termproject.Game;
-
-import android.graphics.Canvas;
-
-public interface GameObject {
-    public void update();
-    public void draw(Canvas canvas);
-}
